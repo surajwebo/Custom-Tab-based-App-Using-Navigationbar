a7fffeff59cf9dc689584068c5f4e19e338f5d5f
surajwebo <jay@Dhananjays-MacBook-Pro.local>
2013-06-11 13:07:41 +0530

    Initial Commit

---
+++ NavBarBased_TabApp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  NavBarBased_TabApp
+//
+//  Created by Dhananjay Vidwans on 11/06/13.
+//  Copyright (c) 2013 suraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ NavBarBased_TabApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NavBarBased_TabApp
+//
+//  Created by Dhananjay Vidwans on 11/06/13.
+//  Copyright (c) 2013 suraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
